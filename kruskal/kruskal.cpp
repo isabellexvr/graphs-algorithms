b961c3fa79d43c9d29b9a899bb3be3244d9c1831
TheMarksan <marcosmelodossantos.com@gmail.com>
2024-11-16 17:57:32 -0300

    fix: kruskal optimization

--- kruskal/kruskal.cpp
+++ kruskal/kruskal.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <queue> // Para usar a fila de prioridade (min-heap)
 #include "grafo.h"
 #include <climits>
-#include <queue>
 #include <fstream>
 #include <limits>
 #include <cstring>
 
 using namespace std;
 
+// Comparador para min-heap (ordem crescente de peso)
+struct ComparadorAresta {
+    bool operator()(const Edge &a, const Edge &b) {
+        return a.peso > b.peso;  // Retorna verdadeiro se 'a' for maior que 'b' (min-heap)
+    }
+};
+
 class UnionFind {
 private:
-    std::vector<int> parent;
-    std::vector<int> rank;
+    vector<int> parent;
+    vector<int> rank;
 
 public:
     UnionFind(int n) {
@@ -48,18 +55,15 @@ public:
     }
 };
 
-
-void help(){
+void help() {
     cout << "-h : mostra o help" << endl;
-    cout << "-o <arquivo> : redireciona a saida para o ‘‘arquivo’’" << endl;
+    cout << "-o <arquivo> : redireciona a saída para o ‘‘arquivo’’" << endl;
     cout << "-f <arquivo> : indica o ‘‘arquivo’’ que contém o grafo de entrada" << endl;
     cout << "-s : mostra a solução" << endl;
-    cout << "-i : vértice inicial";
+    cout << "-i : vértice inicial" << endl;
 }
 
-
-void readInputFile(const string &inputFileName, const string &outputFileName, bool s){
-
+void readInputFile(const string &inputFileName, const string &outputFileName, bool s) {
     ifstream inputFile(inputFileName);
     ofstream outputFile(outputFileName);
 
@@ -68,86 +72,84 @@ void readInputFile(const string &inputFileName, const string &outputFileName, bo
 
     Grafo adj(n);
 
+    // Usando uma fila de prioridade (min-heap) para ordenar as arestas
+    priority_queue<Edge, vector<Edge>, ComparadorAresta> minHeap;
+
     int u, v, peso;
     while (inputFile >> u >> v >> peso) {
+        // Adiciona aresta ao grafo
         adj.add_edge(u, v, peso);
+
+        Edge novaAresta(u, v, peso);
+        minHeap.push(novaAresta);  // Insere a aresta na min-heap
     }
 
-    adj.ordernarArestas();
-
-    auto edges = adj.get_edges(); 
+    // Continua com o código normal de processamento do MST...
     Grafo mst(n);
     UnionFind uf(n);
 
     int custo_total = 0;
 
-    // Adiciona arestas ao MST
-    for (size_t i = 0; i < edges.size(); ++i) {
-        const auto& edge = edges[i];
-        
+    // Processa as arestas da min-heap (extração da aresta com menor peso)
+    while (!minHeap.empty()) {
+        Edge aresta = minHeap.top();
+        minHeap.pop();
+
         // Verifica se a adição da aresta formaria um ciclo
-        if (uf.find(edge.u) != uf.find(edge.v)) {
-            mst.add_edge(edge.u, edge.v, edge.peso); // Adiciona a aresta ao MST
-            uf.union_sets(edge.u, edge.v); // Une os conjuntos
-            custo_total += edge.peso;
+        if (uf.find(aresta.u) != uf.find(aresta.v)) {
+            mst.add_edge(aresta.u, aresta.v, aresta.peso); // Adiciona a aresta ao MST
+            uf.union_sets(aresta.u, aresta.v); // Une os conjuntos
+            custo_total += aresta.peso;
         }
         // Caso contrário, a aresta é ignorada (não é adicionada ao MST)
     }
 
-    if(s){
+    // Mostra a solução ou escreve no arquivo de saída
+    if (s) {
         for (const auto& edge : mst.get_edges()) {
             outputFile << "(" << edge.u << "," << edge.v << ") ";
-            std::cout << "(" << edge.u << "," << edge.v << ") ";
+            cout << "(" << edge.u << "," << edge.v << ") ";
         }
-        cout<<endl;
-    }else{
-        outputFile<<custo_total;
-        cout<<custo_total<<endl;        
+        cout << endl;
+    } else {
+        outputFile << custo_total;
+        cout << custo_total << endl;
     }
-
 
     inputFile.close();
     outputFile.close();
 }
 
 
-int main(int argc, char const *argv[])
-{
-
+int main(int argc, char const *argv[]) {
     string outputFileName, inputFileName;
     int initialVertex = 1, showSolution = false; // vértice inicial vindo do user pelo "-i"
 
-    for (int i = 1; i < argc; i++){
-
-        if(strcmp(argv[i], "-h") == 0){
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
             help();
-
-        }else if(strcmp(argv[i], "-o") == 0){
+        } else if (strcmp(argv[i], "-o") == 0) {
             outputFileName = argv[i + 1];
-
-        }else if(strcmp(argv[i], "-f") == 0){
+        } else if (strcmp(argv[i], "-f") == 0) {
             inputFileName = argv[i + 1];
-
-        } else if(strcmp(argv[i], "-i")==0){
-            initialVertex = stoi(argv[i+1]);
+        } else if (strcmp(argv[i], "-i") == 0) {
+            initialVertex = stoi(argv[i + 1]);
             cout << initialVertex << endl;
-        }else if(strcmp(argv[i], "-s")==0){
+        } else if (strcmp(argv[i], "-s") == 0) {
             showSolution = true;
         }
     }
 
-    if(outputFileName.empty()){
+    if (outputFileName.empty()) {
         outputFileName = "output.bin";
     }
 
-
-    if(inputFileName.empty()){
-        std::cerr << "No input file was specified." << std::endl;
+    if (inputFileName.empty()) {
+        cerr << "No input file was specified." << endl;
         return 1;
-    }else{ 
+    } else {
         readInputFile(inputFileName, outputFileName, showSolution);
     }
-
 
     return 0;
 }
